bd994fb25faaef72c5c8c1b62c635b859b88ccc7
Lordmau5 <dustin.hecken@web.de>
2014-12-12 11:31:14 +0100

    Changes! - Radio buttons -> Checkboxes (no "All" selection anymore) - Added Better Twitch TV emotes - Added "Glorious PC Master Race" category (2 emotes so far, disable it if you don't want it, thought it was a fun addition :P)

--- Kappa Everywhere/contentscript.js
+++ Kappa Everywhere/contentscript.js
@@ -4,114 +4,132 @@
 //TODO: get an icon that's not copywritten? if twitch mentions it. i'm not making money off this so...
 
 // some default settings
-all = false;
-only_globals = true;
-only_subs = false;
-only_kappa = false;
+kappa = false;
+globals = true;
+subs = false;
+bttv = false;
+masterrace = false;
 
-emote_dict = {};
+emote_dict = new Array();
 
 // get settings -> run the program
 chrome.storage.sync.get({
-    all: false,
-    only_globals: true,
-    only_subs: false,
-    only_kappa: false,
+    kappa: false,
+    globals: true,
+    subs: false,
+	bttv: false,
+	masterrace: false,
 },function(items) {
-    all = items.all;
-    only_globals = items.only_globals;
-    only_subs = items.only_subs;
-    only_kappa = items.only_kappa;
+    kappa = items.kappa;
+    globals = items.globals;
+    subs = items.subs;
+	bttv = items.bttv;
+	masterrace = items.masterrace;
     replace_words();
 });
 
 function replace_words() {
     //"if there's no cached data" "or the data is a week old" "or if i goddamn tell you to remotely"
-    function callback() {
-        dfs(document.body);
-        document.addEventListener('DOMNodeInserted', dynamically_replace, false);
+    if (kappa) {
+        get_kappa();
     }
-    if (all) {
-        get_all(callback);
+    if (globals) {
+        get_globals();
     }
-    if (only_globals) {
-        get_globals(callback);
+    if (subs) {
+        get_subs();
     }
-    if (only_subs) {
-        get_subs(callback);
-    }
-    if (only_kappa) {
-        get_kappa(callback);
-    }
+	if (bttv) {
+		get_bttv();
+	}
+	if (masterrace) {
+		get_masterrace();
+	}
     //"else, get it from some sort of cache" <- chrome storage api? limits and size and type (can dicts be values? do i need to json stringify it? Will that fit in chrome storage?)
 }
 
+function doDFS(evt) {
+	dfs(document.body);
+}
+document.addEventListener('replaceWords', doDFS, false);
+document.addEventListener('DOMNodeInserted', dynamically_replace, false);
+
+//emotes to ignore
+ignoreEmotes = ['some'];
+disallowedChars = ['\\', ':', '/', '&', '\'', '"', '?', '!', '#'];
+
 //sub "emote" names to ignore
-ignorelist = ['Win', 'Lose', 'GG', 'Kill', 'IMBA', 'CA', 'US', 'Pylon', 'Gosu', 'Fighting', 'Cheese', 'TW', 'KR','SG','NL','JP','HK','double','triple','SNIPE','SK', 'POISON','C9','inverse','Anubis','Fraud','COAST']
+ignorelist = ['Win', 'Lose', 'GG', 'Kill', 'IMBA', 'CA', 'US', 'Pylon', 'Gosu', 'Fighting', 'Cheese', 'TW', 'KR','SG','NL','JP','HK','double','triple','SNIPE','SK', 'POISON','C9','inverse','Anubis','Fraud','COAST','ICEFROG', 'Ra', 'Apollo','Roshan']
 
-xhr = new XMLHttpRequest();
+dfsEvent = document.createEvent("Event");
+dfsEvent.initEvent('replaceWords', true, true);
+	
+function containsDisallowedChar(word) {
+	for(dis in disallowedChars)
+		if(word.indexOf(dis) > -1)
+			return true;
+	return false;
+}
+	
+function get_kappa() {
+    emote_dict['Kappa'] = {url:'//static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-ddc6e3a8732cb50f-25x28.png'};
+    document.dispatchEvent(dfsEvent);
+    return emote_dict;
+}
 
-function get_all(callback) {
+function get_globals() {
+	var xhr = new XMLHttpRequest();
     xhr.open('GET', '//twitchemotes.com/global.json');
     xhr.send();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-            emote_dict = JSON.parse(xhr.responseText);
-            //get the sub emotes as well as the global emotes
-            xhr.open('GET', '//twitchemotes.com/subscriber.json');
-            xhr.send();
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState == 4) {
-                    emote_d = JSON.parse(xhr.responseText);
-                    for (var key in emote_d) {
-                        for (var key2 in emote_d[key]['emotes']) {
-                            if (ignorelist.indexOf(key2) == -1) {
-                                emote_dict[key2] = {url:emote_d[key]['emotes'][key2]};
-                            }
-                        }
-                    }
-                    callback();
-                    return emote_dict;
-                }
-            }
-        }
+    xhr.onload = function() {
+		emote_d = JSON.parse(xhr.responseText);
+		for (var key in emote_d) {
+			emote_dict[key] = {url:emote_d[key]['url']};
+		}
+		document.dispatchEvent(dfsEvent);
+		return emote_dict;
     }
 }
 
-function get_globals(callback) {
-    xhr.open('GET', '//twitchemotes.com/global.json');
-    xhr.send();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-            emote_dict = JSON.parse(xhr.responseText);
-            callback();
-            return emote_dict;
-        }
-    }
-}
-
-function get_subs(callback) {
+function get_subs() {
+	var xhr = new XMLHttpRequest();
     xhr.open('GET', '//twitchemotes.com/subscriber.json');
     xhr.send();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-            emote_d = JSON.parse(xhr.responseText);
-            for (var key in emote_d) {
-                for (var key2 in emote_d[key]['emotes']) {
-                    if (ignorelist.indexOf(key) == -1) {
-                        emote_dict[key2] = {url:emote_d[key]['emotes'][key2]};
-                    }
-                }
-            }
-            callback();
-            return emote_dict;
-        }
+    xhr.onload = function() {
+		emote_d = JSON.parse(xhr.responseText);
+		for (var key in emote_d) {
+			for (var key2 in emote_d[key]['emotes']) {
+				if (ignorelist.indexOf(key) == -1) {
+					emote_dict[key2] = {url:emote_d[key]['emotes'][key2]};
+				}
+			}
+		}
+		document.dispatchEvent(dfsEvent);
+		return emote_dict;
     }
 }
 
-function get_kappa(callback) {
-    emote_dict = {'Kappa':'//static-cdn.jtvnw.net/jtv_user_pictures/chansub-global-emoticon-ddc6e3a8732cb50f-25x28.png'};
-    callback();
+function get_bttv() {
+	var xhr = new XMLHttpRequest();
+    xhr.open('GET', '//cdn.betterttv.net/emotes/emotes.json');
+    xhr.send();
+    xhr.onload = function() {
+        emote_d = JSON.parse(xhr.responseText);
+		for (var key in emote_d) {
+			var word = emote_d[key]['regex'];
+			if(!containsDisallowedChar(word) && ignoreEmotes.indexOf(word) == -1) {
+				emote_dict[emote_d[key]['regex']] = {url:emote_d[key]['url']};
+			}
+		}
+		document.dispatchEvent(dfsEvent);
+		return emote_dict;
+	}
+}
+
+function get_masterrace() {
+    emote_dict['PCMR'] = {url:'//i.imgur.com/Zp9QNt8.png'};
+	emote_dict['GabeN'] = {url:'//i.imgur.com/yDZTEaf.png'};
+    document.dispatchEvent(dfsEvent);
     return emote_dict;
 }
 
@@ -152,7 +170,7 @@ function replace_text(element) {
         //Write the result to the DOM -> remove "Hey Kappa Kappa Hey Kappa Kappa Hey" from the DOM
         for (var i=0; i < len; i++) {
             word = split[i];
-            if (emote_dict.hasOwnProperty(word)) {
+            if (word in emote_dict && emote_dict[word]['url'] != undefined) {
                 found = true;
                 img = document.createElement('img');
                 //img.src = 'http:' + emote_dict[word]['url'];
