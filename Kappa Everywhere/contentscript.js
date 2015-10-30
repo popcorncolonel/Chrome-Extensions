9b7ecfd058626abe66bd7ff108922a08c6bcc9da
Eric Bailey <popcorncolonel@gmail.com>
2015-10-30 11:48:24 -0400

    Added analytics

--- Kappa Everywhere/contentscript.js
+++ Kappa Everywhere/contentscript.js
@@ -41,6 +41,17 @@ chrome.storage.sync.get({
     }
 
     replace_words();
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-69486347-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = 'https://ssl.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
 });
 
 // One for each setting; so it doesn't dfs 4 times (dfs is pretty slow for huge webpages)
